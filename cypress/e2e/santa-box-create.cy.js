15d3f8b9a05f437984ec05440cca5eed55a6a04e
Maria Shevchenko <shevtma@yandex.ru>
2023-01-14 14:57:44 +0300

    refactored after-hook

--- cypress/e2e/santa-box-create.cy.js
+++ cypress/e2e/santa-box-create.cy.js
@@ -34,7 +34,6 @@ describe("user can create a box and run it", () => {
       .invoke("val")
       .then((boxId) => {
         newBoxId = boxId;
-        cy.log(newBoxId);
       });
     cy.get(generalElements.arrowRight).click();
     cy.get(boxPage.sixthIcon).click();
@@ -137,18 +136,12 @@ describe("user can create a box and run it", () => {
   after("delete box", () => {
     cy.visit("/login");
     cy.login(users.userAuthor.email, users.userAuthor.password);
-    cy.get(
-      '.layout-1__header-wrapper-fixed > .layout-1__header > .header > .header__items > .layout-row-start > [href="/account/boxes"] > .header-item > .header-item__text > .txt--med'
-    ).click();
-    cy.get(":nth-child(1) > a.base--clickable > .user-card").first().click();
-    cy.get(
-      ".layout-1__header-wrapper-fixed > .layout-1__header-secondary > .header-secondary > .header-secondary__right-item > .toggle-menu-wrapper > .toggle-menu-button > .toggle-menu-button--inner"
-    ).click();
-    cy.contains("Архивация и удаление").click({ force: true });
-    cy.get(":nth-child(2) > .form-page-group__main > .frm-wrapper > .frm").type(
-      "Удалить коробку"
-    );
-    cy.get(".btn-service").click();
+    cy.request({
+      method: "DELETE",
+      url: "api/box/" + newBoxId,
+    }).should((response) => {
+      expect(response.status).to.eq(200);
+    });
   });
 });
 
